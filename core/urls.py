6052252b57b8ea490d96cca2b83659a9b73d0a29
tim646 <646botirovtemur@gmail.com>
2023-04-27 17:11:34 +0500

    initial commit: project setup

---
+++ core/urls.py
@@ -0,0 +1,41 @@
+from captcha.fields import ReCaptchaField
+from captcha.widgets import ReCaptchaV2Checkbox
+from django import forms
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.auth.forms import AuthenticationForm
+from django.urls import path
+
+from core.schema import swagger_urlpatterns
+
+
+def trigger_error(request):
+    division_by_zero = 1 / 0  # noqa
+
+
+class LoginForm(AuthenticationForm):
+    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        captcha = cleaned_data.get("captcha")
+        if not captcha:
+            raise forms.ValidationError("Invalid captcha")
+        return cleaned_data
+
+
+admin.site.login_form = LoginForm
+admin.site.login_template = "admin_login/login.html"
+
+urlpatterns = [
+    path("api/v1/sentry/TriggerError", trigger_error),
+    path("admin/", admin.site.urls),
+    # path("api/v1/users/", include("apps.users.urls")),
+]
+
+urlpatterns += swagger_urlpatterns
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
